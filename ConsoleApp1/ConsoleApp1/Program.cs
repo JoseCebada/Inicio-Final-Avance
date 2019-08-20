69cb962bf90f878422a93b2b77be66487ceeaa6f
Alumno <Alumno@PC27>
2019-08-20 10:16:18 -0500

    Ocupa cambios

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+            Console.WriteLine("Imprime los números");
+            Console.WriteLine("De donde arrancamos el ciclo: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("De donde terminamos el ciclo: ");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Como ira avanzando?");
+            z = Convert.ToInt32(Console.ReadLine());
+            if (x > y)
+            {
+                for (int i = x; i <= y; i += z)
+                {
+                    Console.WriteLine("i = " + i);
+                }
+            }
+            else if (x < y)
+            {
+                for (int i = x; i >= y; i -= z)
+                {
+                    Console.WriteLine("i = " + i);
+                }
+            }
+            else
+            {
+                Console.WriteLine("i = " + x);
+            }
+        }
+    }
+}
